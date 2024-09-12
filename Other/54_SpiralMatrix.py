d880ab58b89a00dbc0a433e0cdaaa3a05098873a
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-09-12 14:31:36 -0400

    Create 54_SpiralMatrix.py

---
+++ Other/54_SpiralMatrix.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        output = []
+        left = 0
+        right = len(matrix[0])
+        top = 0
+        bottom = len(matrix)
+
+        while left < right and top < bottom:
+            for i in range(left, right):
+                output.append(matrix[top][i])
+            top += 1
+            
+            for i in range(top, bottom):
+                output.append(matrix[i][right - 1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
+
+            for i in range(right - 1, left - 1, -1):
+                output.append(matrix[bottom - 1][i])
+            bottom -= 1
+
+            for i in range(bottom - 1, top - 1, -1):
+                output.append(matrix[i][left])
+            left += 1
+            
+        return output
