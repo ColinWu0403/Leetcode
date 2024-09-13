ad75d7d891bb060bafb82b97bd97d21e366cc808
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-09-13 15:55:47 -0400

    Create 2095_DeleteMiddleNodeofLinkedList.py

---
+++ Leetcode 75/2095_DeleteMiddleNodeofLinkedList.py
@@ -0,0 +1,37 @@
+"""
+You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+    For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteMiddle(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not head or not head.next:
+            return None
+
+        # Initialize pointers
+        slow = head
+        fast = head
+        prev = None
+        
+        # Move fast pointer by 2 steps and slow pointer by 1 step
+        while fast and fast.next:
+            fast = fast.next.next
+            prev = slow
+            slow = slow.next
+        
+        # Remove the middle node
+        if prev:
+            prev.next = slow.next
+        
+        return head
