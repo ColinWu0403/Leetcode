2c53f021b3ebe1ab962654c15ee2411238a4a7e6
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-09-27 16:55:35 -0400

    Create 729_MyCalendarI.py

---
+++ Daily/729_MyCalendarI.py
@@ -0,0 +1,33 @@
+"""
+You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+Implement the MyCalendar class:
+
+    MyCalendar() Initializes the calendar object.
+    boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+"""
+class MyCalendar(object):
+
+    def __init__(self):
+        self.events = []
+
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        for start_cur, end_cur in self.events:
+            if not (end <= start_cur or start >= end_cur):
+                return False
+        self.events.append((start, end))
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
