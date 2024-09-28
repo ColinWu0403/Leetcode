789cf81306aa0d58a94a8d895dce3aaa28d5f885
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-09-28 18:26:23 -0400

    Create 169_MajorityElement.cs

---
+++ Other/169_MajorityElement.cs
@@ -0,0 +1,31 @@
+/**
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+**/
+
+public class Solution {
+
+    // O(n) Time and O(n) Space Solution
+    public int MajorityElement(int[] nums) {
+        // Using Dictionary as Hashmap
+        Dictionary<int, int> numCounts = new Dictionary<int, int>();
+        int result = 0;
+        int maxCount = 0;
+        foreach (int num in nums) {
+            if (!numCounts.ContainsKey(num)) { // key doesn't exist
+                numCounts[num] = 0;
+            }
+
+            numCounts[num] += 1;
+
+            // Update result if element appears more
+            if (numCounts[num] > maxCount) {
+                maxCount = numCounts[num];
+                result = num;
+            }
+        }
+
+        return result;
+    }
+}
